fc652f6c602e668b0a65fe4a84c90e9751c593c9
borossandor27 <boros.sanyi@gmail.com>
2025-11-24 21:46:10 +0100

    1.42

--- 60-Context-Roles/src/pages/UserProfile.jsx
+++ 60-Context-Roles/src/pages/UserProfile.jsx
@@ -1,15 +1,19 @@
+import { useAuth } from "../components/AuthContext.jsx";
 const UserProfile = () => {
-    const { user, logout } = useAuth();
-    return (    
-        <div>
-            <h1>Üdvözöllek, {user.userName}!</h1>
-            <p>Felhasználói azonosító: {user.userId}</p>
-            <p>Szerepkör: {user.role}</p>
-            <p>Email: {user.userEmail}</p>
-            <button onClick={logout}>Kijelentkezés</button>
-            <p>Ez a felhasználói profil oldal, amely csak bejelentkezett felhasználók számára érhető el.</p>
-        </div>
-    );
-}   
+  const { user, logout } = useAuth();
+  return (
+    <div>
+      <h1>Üdvözöllek, {user.userName}!</h1>
+      <p>Felhasználói azonosító: {user.userId}</p>
+      <p>Szerepkör: {user.role}</p>
+      <p>Email: {user.userEmail}</p>
+      <button onClick={logout}>Kijelentkezés</button>
+      <p>
+        Ez a felhasználói profil oldal, amely csak bejelentkezett felhasználók
+        számára érhető el.
+      </p>
+    </div>
+  );
+};
 
 export default UserProfile;
