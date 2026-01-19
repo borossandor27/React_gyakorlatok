4d80407a228bec1e378e843d91038ea734194b7e
borossandor27 <boros.sanyi@gmail.com>
2026-01-19 12:11:02 +0100

    1.7

---
+++ 04-useContext/ChangeLanguage/src/layouts/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Change Language Example</h1>
+            <div className="flags">
+                <img src="/flags/Flag_of_Hungary.png" alt="Hungarian Flag" width="50" />
+                <img src="/flags/Flag_of_United_Kingdom.png" alt="UK Flag" width="50" />
+                <img src="/flags/Flag_of_Germany.png" alt="German Flag" width="50" />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
